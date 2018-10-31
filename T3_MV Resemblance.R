526e631948521797915047e77da8cedddad323bc
Jonathan Batchelor <40809985+JonBatch@users.noreply.github.com>
2018-10-31 01:13:51 -0700

    update

--- T3_MV Resemblance.R
+++ T3_MV Resemblance.R
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 setwd("D:/GIT/R-code-for-multivariate-analysis")
 source("biostats.R")
 install.packages("simba")
@@ -13,19 +14,37 @@ library("cluster")
 envdata<-read.csv('MAHA_environment.csv', header = TRUE, row.names = 1)
 speabu <- read.csv('MAHA_speciesabu.csv', header=TRUE, row.names = 1)
 spetrait<-read.csv('MAHA_speciestrait.csv', header=TRUE, row.names=1)
-=======
-setwd("D:/GIT/R-code-for-multivariate-analysis")
-source("biostats.R")
-install.packages("simba")
-install.packages("vegan")
-install.packages("ecodist")
-install.packages("cluster")
-library("simba")
-library("vegan")
-library("ecodist")
-library("cluster")
 
-envdata<-read.csv('MAHA_environment.csv', header = TRUE, row.names = 1)
-speabu <- read.csv('MAHA_speciesabu.csv', header=TRUE, row.names = 1)
-spetrait<-read.csv('MAHA_speciestrait.csv', header=TRUE, row.names=1)
->>>>>>> 86b915e4447d6661a7a0687d03658c40a681fb27
+speocc<-data.trans(speabu,method = 'power', exp=0, plot = F)
+
+sp.jac<-sim(speocc,method="jaccard")
+sp.jac
+sp.sim<-sim(speocc,method = "simplematching")
+sp.sor<-sim(speocc,method = "soerensen")
+plot(sp.jac,sp.sim, xlab="Jaccard's coefficient", ylab="Simple Matching coefficient")
+abline(0,1,col="darkgray")
+
+sp.bray <- vegdist(speabu, method="bray")
+sp.bray
+
+install.packages("gclus")
+library("gclus")
+source(coldiss.R)
+
+coldiss(sp.bray,nc=4, byrank=FALSE, diag=TRUE)
+
+sp.jacd<-vegdist(speocc,method = "jaccard")
+plot(sp.jac, 1-sp.jacd)
+abline(0,1,col="darkgray")
+
+speabu.norm<-decostand(speabu, method= 'nor')
+sp.chordd<-dist(speabu.norm)
+
+speabu.hel<-decostand(speabu, method='hel')
+sp.held<-dist(speabu.hel)
+
+str(spetrait)
+sptr.gower <-daisy(spetrait,metric = "gower")
+coldiss(sptr.gower,nc=4,byrank=FALSE,diag=TRUE)
+env.euc<-vegdist(envdata, method="euclidean")
+env.euc
